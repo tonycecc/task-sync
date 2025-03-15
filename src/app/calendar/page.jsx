5b817614b5e04c1c5e1e52f5e1b03ae056159fd2
RyeleighAvila <163746776+RyeleighAvila@users.noreply.github.com>
2025-03-15 00:23:41 -0500

    feat: Name change to "Calendar"

---
+++ src/app/calendar/page.jsx
@@ -0,0 +1,5 @@
+export default function Calendar() {
+    return (
+        <h1>Welcome to the Calendar</h1>
+    )
+}
--- src/app/schedule/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function Schedule() {
-    return (
-        <h1>Welcome to Schedule</h1>
-    )
-}
