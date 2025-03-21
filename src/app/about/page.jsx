844709bdadca39ef768a6baa25e75a25ac2a6ced
lzherrera <lzherrera03@gmail.com>
2025-03-21 01:14:44 -0500

    Minor Space Changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,16 +1,30 @@
 export default async function AboutPage() {
   return (
     <div classname="text-center font-bold text-2xl">About Us
+      <p>
+      </p>
       <p> TaskSync started out as a software project for our Senior Capstone Class (at one point we wanted to call it Agenda Ninja, but that was scrapped for respectibility's sake).
         [Editor's Note: I still wanted it though]
-        Anyway, we hope your team can get some use out of this project. 
-        Sincerely,
-        Khoi Le
-        Ryeleigh Avila
-        Logan Herrera
-        Anthony Cecchini
-        
-        p.s. Thank you to our instructor, Asaad Althoubi, for helping us out along the way.
+      </p>
+      <p>
+      </p>
+      <p> Anyway, we hope your team can get some use out of this project. 
+      </p>
+      <p>
+      </p>
+      <p>     Sincerely,
+      </p>
+      <p>Khoi Le
+      </p>
+      <p>Ryeleigh Avila
+      </p>
+      <p>Logan Herrera
+      </p>
+      <p>Anthony Cecchini
+      </p>
+      <p>
+      </p>
+      <p>p.s. Thank you to our instructor, Asaad Althoubi, for helping us out along the way.
       </p>
     </div>
   );
